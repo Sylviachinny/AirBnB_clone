6ef538ce8b01b0a43f5e3fdf9556ce0a1de0d157
Sylviachinny.sylvia <Sylviachinny.chinyeresylvia222@gmail.com>
2023-12-11 03:44:04 -0800

    AirBnB console

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+"""The import modules"""
 import uuid
 from datetime import datetime
+
 
 class BaseModel():
     """The class Basemodel"""
@@ -14,15 +16,22 @@ class BaseModel():
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """using __str__ representation to print
+        """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """The public instance method
+        that saves the updated datetime
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """using dict_copying to contain
+        all keys/values
+        """
         dict_copying = self.__dict__.copy()
         dict_copying["__class__"] = self.__class__.__name__
         dict_copying["created_at"] = self.created_at.isoformat()
         dict_copying["updated_at"] = self.updated_at.isoformat()
         return dict_copying
-
